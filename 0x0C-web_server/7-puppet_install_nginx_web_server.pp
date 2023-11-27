4278c196f030beed2bc6d830e5f3ff3298a4da73
Nivesh <105005447+Nivesh30@users.noreply.github.com>
2023-11-27 21:44:23 +0200

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,5 +1,37 @@
-# Install Nginx web server (w/ Puppet) 
- exec { 'server configuration': 
-   provider => shell, 
-   command  => 'sudo apt-get -y update; sudo apt-get -y install nginx; echo "Hello World!" > /var/www/html/index.html; sudo sed -i "/server_name _;/a location /redirect_me {\\n\\treturn 301 https://google.com; listen 80; \\n\\t}\\n" /etc/nginx/sites-available/default; sudo service nginx restart' 
- }
+# Define Nginx class
+class { 'nginx':
+  manage_repo => true,
+  listen_port => 80,
+}
+
+# Set up default Nginx vhost
+nginx::resource::vhost { 'default':
+  www_root     => '/var/www/html',
+  listen_port  => 80,
+  index_files  => ['index.html'],
+}
+
+# Create Hello World index page
+file { '/var/www/html/index.html':
+  ensure  => present,
+  content => 'Hello World!',
+}
+
+# Set up redirection for /redirect_me
+nginx::resource::vhost { 'redirect_me':
+  ensure       => present,
+  www_root     => '/var/www/html',
+  listen_port  => 80,
+  server_name  => 'localhost',
+  locations    => {
+    '/' => {
+      ensure       => present,
+      location     => '/redirected_page',
+      return       => '301',
+    },
+    '/redirected_page' => {
+      ensure       => present,
+      content      => 'Hello, this is the redirected page!',
+    },
+  },
+}
