8d894df3f32ae10d8e2053114428a8d8f94ee0fb
Nivesh <105005447+Nivesh30@users.noreply.github.com>
2023-11-27 21:45:31 +0200

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,37 +1,45 @@
-# Define Nginx class
-class { 'nginx':
-  manage_repo => true,
-  listen_port => 80,
+# Install Nginx package
+package { 'nginx':
+  ensure => installed,
 }
 
-# Set up default Nginx vhost
-nginx::resource::vhost { 'default':
-  www_root     => '/var/www/html',
-  listen_port  => 80,
-  index_files  => ['index.html'],
+# Ensure Nginx service is running and enabled
+service { 'nginx':
+  ensure => running,
+  enable => true,
 }
 
-# Create Hello World index page
+# Configure Nginx server block for default site
+file { '/etc/nginx/sites-available/default':
+  ensure  => present,
+  content => '
+    server {
+      listen 80 default_server;
+      listen [::]:80 default_server;
+
+      root /var/www/html;
+      index index.html;
+
+      location / {
+        try_files $uri $uri/ =404;
+        add_header X-Hello-World "Hello World!";
+      }
+
+      location /redirect_me {
+        return 301 https://www.example.com/redirected_page;
+      }
+
+      location /redirected_page {
+        return 200 "Hello, this is the redirected page!";
+      }
+    }
+  ',
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+# Create HTML file for the default page
 file { '/var/www/html/index.html':
   ensure  => present,
   content => 'Hello World!',
-}
-
-# Set up redirection for /redirect_me
-nginx::resource::vhost { 'redirect_me':
-  ensure       => present,
-  www_root     => '/var/www/html',
-  listen_port  => 80,
-  server_name  => 'localhost',
-  locations    => {
-    '/' => {
-      ensure       => present,
-      location     => '/redirected_page',
-      return       => '301',
-    },
-    '/redirected_page' => {
-      ensure       => present,
-      content      => 'Hello, this is the redirected page!',
-    },
-  },
 }
